7cb6c8a7eef3aec144741370ae8da25682a24334
carolahp <carola.hp@gmail.com>
2019-10-03 17:52:46 +0200

    setting up version for PBManager

--- bootstrap/scripts/2-download.sh
+++ bootstrap/scripts/2-download.sh
@@ -11,7 +11,6 @@ cd ${SCRIPTS}/pharo
 
 ${SCRIPTS}/getPharoVM.sh 70
 wget https://github.com/carolahp/PharoBootstrap/releases/download/v2.0.2/bootstrapImage.zip
-#wget https://github.com/carolahp/PharoBootstrap/releases/download/v2.0/bootstrapImage.zip
 unzip -o bootstrapImage.zip
 wget https://github.com/carolahp/PharoCandleSrc/archive/v1.0.zip
 unzip -o v1.0 
